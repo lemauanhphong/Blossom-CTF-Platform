c4dde9a67753f75945aaea52650bf2a124724b19
Le Mau Anh Phong <lemauanhphong@gmail.com>
2023-11-22 15:25:45 +0700

    Remove unnecessary images and fix util navBarItems

--- frontend/src/pages/Challenges.tsx
+++ frontend/src/pages/Challenges.tsx
@@ -3,9 +3,12 @@ import NavBar from "../components/NavBar";
 import { navBarItems } from "../utils";
 
 export default () => {
+    const isLoggedIn = false;
+    const isAdmin = false;
+
     return (
         <>
-            <NavBar items={navBarItems[true]} selectedIndex={2} />
+            <NavBar items={navBarItems(isLoggedIn, isAdmin)} selectedIndex={2} />
             <Challenges />
         </>
     );
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,15 +1,28 @@
-export const navBarItems = {
-    false: [
+export const navBarItems = (isLoggedIn?: boolean, isAadmin?: boolean) => {
+    if (!isLoggedIn)
+        return [
+            { name: "🌼Home", route: "/" },
+            { name: "Scoreboard", route: "/scoreboard" },
+            { name: "Challenges", route: "/challenges" },
+            { name: "Register", route: "/register" },
+            { name: "Login", route: "/login" },
+        ];
+
+    if (isAadmin)
+        return [
+            { name: "🌼Home", route: "/" },
+            { name: "Scoreboard", route: "/scoreboard" },
+            { name: "Challenges", route: "/challenges" },
+            { name: "Profile", route: "/profile" },
+            { name: "Admin", route: "/admin" },
+            { name: "Logout", route: "/logout" },
+        ];
+
+    return [
         { name: "🌼Home", route: "/" },
         { name: "Scoreboard", route: "/scoreboard" },
         { name: "Challenges", route: "/challenges" },
-        { name: "Register", route: "/register" },
-        { name: "Login", route: "/login" },
-    ],
-    true: [
-        { name: "🌼Home", route: "/" },
-        { name: "Scoreboard", route: "/scoreboard" },
-        { name: "Challenges", route: "/challenges" },
+        { name: "Profile", route: "/profile" },
         { name: "Logout", route: "/logout" },
-    ],
+    ];
 };
