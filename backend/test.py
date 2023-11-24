03bc24300195026ce44fcdf71dba3019bbaf1030
woanmeo11 <woanmeo11@gmail.com>
2023-11-24 15:26:59 +0700

    update test file

--- backend/test.py
+++ backend/test.py
@@ -12,104 +12,114 @@ def randstr():
     return "".join(random.choices(string.ascii_letters, k=10))
 
 
-def register(account):
-    r = s.post(TARGET + "/register", json=account)
-    return r.json()
+class Api:
+    def __init__(self, username=randstr(), password=randstr()) -> None:
+        self.s = Session()
+        self.account = {"username": username, "password": password}
+
+    def register(self):
+        r = self.s.post(TARGET + "/register", json=self.account)
+        return r.json()
+
+    def login(self):
+        r = self.s.post(TARGET + "/login", json=self.account)
+        return r.json()
+
+    def logout(self):
+        r = self.s.get(TARGET + "/logout")
+        return r.json()
+
+    def change_password(self, new_password):
+        r = self.s.post(
+            TARGET + "/change_password", json={"password": self.account["password"], "new_password": new_password}
+        )
+        response = r.json()
+        self.account["password"] = new_password
+        return response
+
+    def get_challs(self):
+        r = self.s.get(TARGET + "/challs")
+        return r.json()
+
+    def admin_get_challs(self):
+        r = self.s.get(TARGET + "/admin/challs")
+        return r.json()
+
+    def add_chall(self, chall):
+        r = self.s.post(TARGET + "/admin/challs", json=chall)
+        return r.json()
+
+    def update_chall(self, chall):
+        r = self.s.put(TARGET + "/admin/challs", json=chall)
+        return r.json()
+
+    def delete_chall(self, name):
+        r = self.s.delete(TARGET + "/admin/challs", json={"name": name})
+        return r.json()
+
+    def get_current_profile(self):
+        r = self.s.get(TARGET + "/profile")
+        return r.json()
+
+    def get_public_profile(self, _id):
+        r = self.s.get(TARGET + "/profile/" + _id)
+        return r.json()
+
+    def scores(self):
+        r = self.s.get(TARGET + "/scores")
+        return r.json()
+
+    def categories(self):
+        r = self.s.get(TARGET + "/categories")
+        return r.json()
+
+    def submit_flag(self, name, flag):
+        r = self.s.post(TARGET + "/flag", json={"name": name, "flag": flag})
+        return r.json()
+
+    def get_file(self, fileid):
+        r = self.s.get(TARGET + "/files", params={"fileid": fileid})
+        return r.text
 
 
-def login(account):
-    r = s.post(TARGET + "/login", json=account)
-    return r.json()
+def generate_new_challs():
+    return {
+        "name": randstr(),
+        "category": random.choice(["wed", "pown", "4n6", "cryto"]),
+        "content": randstr(),
+        "flag": "flag{%s}" % randstr(),
+        "files": {
+            "a.txt": b64encode(randstr().encode()).decode(),
+            "b.txt": b64encode(randstr().encode()).decode(),
+        },
+        "score": random.randint(0, 1000),
+    }
 
 
-def logout():
-    r = s.get(TARGET + "/logout")
-    return r.json()
+def populate_scoreboard():
+    admin = Api("admin", "admin")
+    admin.login()
+
+    flags = []
+
+    for _ in range(5):
+        new_chall = generate_new_challs()
+        pprint(admin.add_chall(new_chall))
+        flags.append((new_chall["name"], new_chall["flag"]))
+
+    for _ in range(5):
+        user = Api()
+        user.register()
+        user.login()
+        for name, flag in random.choices(flags, k=random.randint(0, len(flags))):
+            pprint(user.submit_flag(name, flag))
+
+    pprint(admin.get_challs())
 
 
-def change_password(password, new_password):
-    r = s.post(TARGET + "/change_password", json={"password": password, "new_password": new_password})
-    return r.json()
+if __name__ == "__main__":
+    populate_scoreboard()
 
-
-def get_challs():
-    r = s.get(TARGET + "/challs")
-    return r.json()
-
-
-def admin_get_challs():
-    r = s.get(TARGET + "/admin/challs")
-    return r.json()
-
-
-def add_chall(chall):
-    r = s.post(TARGET + "/admin/challs", json=chall)
-    return r.json()
-
-
-def update_chall(chall):
-    r = s.put(TARGET + "/admin/challs", json=chall)
-    return r.json()
-
-
-def delete_chall(name):
-    r = s.delete(TARGET + "/admin/challs", json={"name": name})
-    return r.json()
-
-
-def get_current_profile():
-    r = s.get(TARGET + "/profile")
-    return r.json()
-
-
-def get_public_profile(_id):
-    r = s.get(TARGET + "/profile/" + _id)
-    return r.json()
-
-
-def scores():
-    r = s.get(TARGET + "/scores")
-    return r.json()
-
-
-def categories():
-    r = s.get(TARGET + "/categories")
-    return r.json()
-
-
-def submit_flag(name, flag):
-    r = s.post(TARGET + "/flag", json={"name": name, "flag": flag})
-    return r.json()
-
-
-def get_file(fileid):
-    r = s.get(TARGET + "/files", params={"fileid": fileid})
-    return r.text
-
-
-s = Session()
-
-
-account = {"username": randstr(), "password": randstr()}
-account = {"username": "admin", "password": "admin"}
-print(account)
-
-pprint(register(account))
-pprint(login(account))
-print(s.cookies)
-
-chall = {
-    "name": randstr(),
-    "category": random.choice(["wed", "pown", "4n6", "cryto"]),
-    "content": randstr(),
-    "flag": "flag{%s}" % randstr(),
-    "files": {
-        "a.txt": b64encode(randstr().encode()).decode(),
-        "b.txt": b64encode(randstr().encode()).decode(),
-    },
-    "score": random.randint(0, 1000),
-}
-
-pprint(get_current_profile())
-pprint(get_public_profile("655f61cd2b9f5c9c50c1964f"))
-pprint(get_challs())
+    admin = Api("admin", "admin")
+    admin.login()
+    pprint(admin.get_current_profile())
